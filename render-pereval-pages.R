cb8b56d55c72e6c5c9dc57f18cb0cc52f7996c7c
Petr Bouchal <pbouchal@gmail.com>
2023-04-10 21:51:59 +0200

    improve eval page rendering

--- render-pereval-pages.R
+++ render-pereval-pages.R
@@ -3,14 +3,13 @@ library(glue)
 library(dplyr)
 library(stringr)
 library(purrr)
-library(furrr)
+# library(furrr)
 
-plan(multisession)
 
 dat <- readRDS("data-interim/evals_for_table.rds") |>
   mutate(eval_kod = str_sub(etapa_kod, 1, 7) |> str_remove("\\.$"))
 
-render_single_eval <- function(eval_kod, file) {
+render_single_eval <- function(eval_kod, eval_nazev, file) {
   # quarto::quarto_render(file, execute_params = list(kraj = kraj),
   #                       output_file = paste0("kraj_", kraj, ".html"), quiet = TRUE)
   eval_kod_clean <- stringr::str_replace_all(eval_kod, "\\.", "\\-")
@@ -18,8 +17,8 @@ render_single_eval <- function(eval_kod, file) {
 
   args <- c("render", file,
             "--execute-param", glue("eval_kod:{eval_kod}"),
-            # "--metadata", glue("title:{eval_kod}"),
-            # "--metadata", glue("pagetitle:{eval_kod}"),
+            "--metadata", glue("title:{eval_nazev}"),
+            "--metadata", glue("pagetitle:{eval_nazev}"),
             # "--profile", "batch",
             "--output-dir", ".",
             "--output", glue("{output_file}"))
@@ -31,8 +30,13 @@ render_single_eval <- function(eval_kod, file) {
 }
 # evals_out <- map_chr(unique(dat$eval_kod)[1], render_single_eval, "single-eval-template.qmd")
 
+evals_meta <- distinct(dat, eval_kod, eval_nazev)
+
 # fs::dir_delete("evals")
-evals_out <- map_chr(unique(dat$eval_kod), render_single_eval, "single-eval-template.qmd")
-plan(sequential)
+# plan(multisession)
+# evals_out <- future_map2_chr(evals_meta$eval_kod, evals_meta$eval_nazev,
+evals_out <- map2_chr(evals_meta$eval_kod, evals_meta$eval_nazev,
+                             render_single_eval, "single-eval-template.qmd",
+                             .progress = TRUE)
+# plan(sequential)
 # fs::dir_delete("evals/docs")
-unique(dat$eval_kod)[5]
